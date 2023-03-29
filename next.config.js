04c9b06854738943a0a8166f6afe753906452129
thvrijn <thvrijn2002@gmail.com>
2023-03-29 13:06:50 +0200

    added pwe

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+    reactStrictMode: true
 }
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+    dest: 'public'
+})
+
+module.exports = withPWA({
+    reactStrictMode: true
+})
